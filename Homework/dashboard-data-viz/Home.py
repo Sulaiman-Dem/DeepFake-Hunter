4dcd0621c445d17da889b51f713ffea73d1cf714
syi7190 <sungyi456@gmail.com>
2023-09-30 14:13:20 -0400

    .

---
+++ Homework/dashboard-data-viz/Home.py
@@ -0,0 +1,35 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.set_page_config(
+    page_title="Streamlit Dashboard"
+)
+# Create a page header
+st.header("Baseball statistics for both the American League and National League!")
+
+st.divider()
+
+# Create three columns 
+col1, col2= st.columns([1,1])
+
+
+
+# inside of the first column
+with col1:
+
+    # display a picture
+    st.image('images/BaseballAL.png')
+
+    # display the link to that page.
+    st.write('<a href="/American League"> Check out the statistics of the American League!</a>', unsafe_allow_html=True)
+
+# inside of the second column
+with col2:
+    # display another picture
+    st.image('images/BaseballNL.png')
+
+    # display another link to that page
+    st.write('<a href="/National League">Check out the statistics of the American League!</a>', unsafe_allow_html=True)
+
+
---
+++ Homework/dashboard-data-viz/pages/American League.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import pickle
+import plotly.express as px
+
+st.set_page_config(
+    page_title="American League"
+)
