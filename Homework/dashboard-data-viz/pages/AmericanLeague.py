88b509a3dfbc2c009a3996414bbf755c0f42d93a
syi7190 <sungyi456@gmail.com>
2023-10-02 12:39:19 -0400

    a

--- Homework/dashboard-data-viz/pages/AmericanLeague.py
+++ Homework/dashboard-data-viz/pages/AmericanLeague.py
@@ -4,6 +4,24 @@ import numpy as np
 import pickle
 import plotly.express as px
 
+#Page title
 st.set_page_config(
     page_title="American League"
 )
+
+#Loading in data
+df = pd.read_csv('data/baseball.csv')
+Aleague = df[df['League']=='AL'].copy()
+Nleague = df[df['League']=='NL'].copy()
+
+#Creating a separate dataframe to graph the Year and average RS for AL
+AL_y_rs = Aleague.groupby('Year')['RS'].mean().reset_index()
+fig = px.bar(AL_y_rs,
+        x='Year',
+        y='RS', 
+        title="Average Runs Scored per Year.",
+        labels={ "RS": "Average Runs Scored"} 
+        )
+
+# display graph
+st.plotly_chart(fig, use_container_width=True)
--- Homework/dashboard-data-viz/pages/NationalLeague.py
+++ Homework/dashboard-data-viz/pages/NationalLeague.py
@@ -4,6 +4,24 @@ import numpy as np
 import pickle
 import plotly.express as px
 
+#Page title
 st.set_page_config(
     page_title="National League"
 )
+
+#Loading in data
+df = pd.read_csv('data/baseball.csv')
+Aleague = df[df['League']=='AL'].copy()
+Nleague = df[df['League']=='NL'].copy()
+
+#Creating a separate dataframe to graph the Year and average RS for NL
+NL_y_rs = Nleague.groupby('Year')['RS'].mean().reset_index()
+fig = px.bar(NL_y_rs,
+        x='Year',
+        y='RS', 
+        title="Average Runs Scored per Year.",
+        labels={ "RS": "Average Runs Scored"} 
+        )
+
+# display graph
+st.plotly_chart(fig, use_container_width=True)
